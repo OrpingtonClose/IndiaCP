54262868c5b49f04050672e809fa5265b6e5b152
Ritu Kedia <ritukedia@gmail.com>
2016-12-22 18:23:16 +0530

    Added REST APIs for CPProgram and CPIssue

--- IndiaCP_Corda/india-cp-cordapp/src/main/kotlin/com/barclays/indiacp/cordapp/api/IndiaCPProgramApi.kt
+++ IndiaCP_Corda/india-cp-cordapp/src/main/kotlin/com/barclays/indiacp/cordapp/api/IndiaCPProgramApi.kt
@@ -25,12 +25,147 @@ class IndiaCPProgramApi(val services: ServiceHub) {
     }
 
     @POST
-    @Path("generateISIN/{ref}")
+    @Path("issueCPProgram")
     @Consumes(MediaType.APPLICATION_JSON)
-    fun generateISIN(@PathParam("ref") ref: String, isin: String): Response {
+    fun issueCPProgram(indiaCPProgramJSON: IndiaCPProgramJSON): Response {
         try {
-//            val stx = rpc.startFlow(::ISINGenerationFlow, ref, isin).returnValue.toBlocking().first()
-//            logger.info("ISIN Stamped on CP\n\nFinal transaction is:\n\n${Emoji.renderIfSupported(stx.tx)}")
+            val stx = services.invokeFlowAsync(IssueCPProgramFlow::class.java, indiaCPProgramJSON).resultFuture.get()
+            logger.info("CP Program Issued\n\nFinal transaction is:\n\n${Emoji.renderIfSupported(stx.tx)}")
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating CP Program deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("addISINGenerationDocs/{cpProgramId}/{docHashId}/{docStatus}")
+    fun addISINGenerationDocs(@PathParam("cpProgramId") cpProgramId: String,
+                             @PathParam("docHashId") docHashId: String,
+                             @PathParam("docStatus") docStatus: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("addISIN/{cpProgramId}/{isin}")
+    fun addISIN(@PathParam("cpProgramId") cpProgramId: String,
+                             @PathParam("docHashId") docHashId: String,
+                             @PathParam("docStatus") docStatus: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("issueCP/{cpProgramId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun issueCP(@PathParam("ref") ref: String, isin: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("addIPAVerificationDocs/{cpProgramId}/{docHashId}/{docStatus}")
+    fun addIPAVerificationDocs(@PathParam("cpProgramId") cpProgramId: String,
+                             @PathParam("docHashId") docHashId: String,
+                             @PathParam("docStatus") docStatus: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("addIPACertifcateDoc/{cpProgramId}/{docHashId}/{docStatus}")
+    fun addIPACertifcateDoc(@PathParam("cpProgramId") cpProgramId: String,
+                               @PathParam("docHashId") docHashId: String,
+                               @PathParam("docStatus") docStatus: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("addCorpActionFormDoc/{cpProgramId}/{docHashId}/{docStatus}")
+    fun addCorpActionFormDoc(@PathParam("cpProgramId") cpProgramId: String,
+                            @PathParam("docHashId") docHashId: String,
+                            @PathParam("docStatus") docStatus: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("addAllotmentLetterDoc/{cpProgramId}/{docHashId}/{docStatus}")
+    fun addAllotmentLetterDoc(@PathParam("cpProgramId") cpProgramId: String,
+                             @PathParam("docHashId") docHashId: String,
+                             @PathParam("docStatus") docStatus: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("closeCPProgram/{cpProgramId}")
+    fun closeCPProgram(@PathParam("cpProgramId") cpProgramId: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("getTransactionHistory/{cpProgramId}")
+    fun getTransactionHistory(@PathParam("cpProgramId") cpProgramId: String): Response {
+        try {
+            //TODO: Add code here
+            return Response.status(Response.Status.OK).build()
+        } catch (ex: Throwable) {
+            logger.info("Exception when creating deal: ${ex.toString()}")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
+        }
+    }
+
+    @POST
+    @Path("getDocumentHistory/{cpProgramId}/{docType}/{docSubType}")
+    fun getDocumentHistory(@PathParam("cpProgramId") cpProgramId: String,
+                           @PathParam("docType") docType: String,
+                           @PathParam("docSubType") docSubType: String): Response {
+        try {
+            //TODO: Add code here
             return Response.status(Response.Status.OK).build()
         } catch (ex: Throwable) {
             logger.info("Exception when creating deal: ${ex.toString()}")
@@ -68,20 +203,6 @@ class IndiaCPProgramApi(val services: ServiceHub) {
         return if (states.isEmpty()) null else {
             val deals = states.values.map { it.state.data }
             return if (deals.isEmpty()) null else deals[0]
-        }
-    }
-
-    @POST
-    @Path("issueCPProgram")
-    @Consumes(MediaType.APPLICATION_JSON)
-    fun issueCPProgram(indiaCPProgramJSON: IndiaCPProgramJSON): Response {
-        try {
-            val stx = services.invokeFlowAsync(IssueCPProgramFlow::class.java, indiaCPProgramJSON).resultFuture.get()
-            logger.info("CP Program Issued\n\nFinal transaction is:\n\n${Emoji.renderIfSupported(stx.tx)}")
-            return Response.status(Response.Status.OK).build()
-        } catch (ex: Throwable) {
-            logger.info("Exception when creating CP Program deal: ${ex.toString()}")
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.toString()).build()
         }
     }
 
