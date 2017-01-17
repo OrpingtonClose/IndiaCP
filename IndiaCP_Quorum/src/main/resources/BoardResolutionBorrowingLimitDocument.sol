7054af87c7723036f80fe5d4df2f958b200348a3
nikhil <nv1620@gmail.com>
2017-01-17 21:26:24 +0530

    add CR BR solidity code

---
+++ IndiaCP_Quorum/src/main/resources/BoardResolutionBorrowingLimitDocument.sol
@@ -0,0 +1,51 @@
+contract BoardResolutionBorrowingLimitDocument{
+
+ string legalEntityId;
+ uint boardResolutionBorrowingLimit;
+ uint currentOutstandingCreditBorrowing;
+ string currency;
+ string docHash;
+
+ uint boardResolutionIssuanceDate;
+ uint boardResolutionExpiryDate;
+
+ uint version;
+ string modifiedBy;
+ uint lastModifiedDate;
+ string status;
+
+ function BoardResolutionBorrowingLimitDocument(string _legalEntityId, uint _boardResolutionBorrowingLimit,
+    uint _boardResolutionIssuanceDate, uint _boardResolutionExpiryDate,
+    string _docHash){
+
+    legalEntityId=_legalEntityId;
+    boardResolutionBorrowingLimit=_boardResolutionBorrowingLimit;
+    boardResolutionIssuanceDate=_boardResolutionIssuanceDate;
+    boardResolutionExpiryDate=_boardResolutionExpiryDate;
+    docHash=_docHash;
+
+    //default values
+
+    currentOutstandingCreditBorrowing=0;
+    version=0;
+    currency="INR";
+    lastModifiedDate=now;
+    status="ACTIVE";
+
+ }
+
+
+
+ function fetchCRDetails() constant returns(string _legalEntityId, uint _boardResolutionBorrowingLimit, uint _currentOutstandingCreditBorrowing, string _currency, string _docHash, uint _boardResolutionIssuanceDate, uint _boardResolutionExpiryDate){
+
+    return (legalEntityId, boardResolutionBorrowingLimit, currentOutstandingCreditBorrowing, currency, docHash, boardResolutionIssuanceDate, boardResolutionExpiryDate);
+
+ }
+
+ function fetchCRStatus() constant returns(uint _version, string _modifiedBy, uint _lastModifiedDate, string _status){
+
+    return (version, modifiedBy, lastModifiedDate, status);
+ }
+
+}
+
---
+++ IndiaCP_Quorum/src/main/resources/LegalEntityCreditRatingDocument.sol
@@ -0,0 +1,60 @@
+contract LegalEntityCreditRatingDocument{
+
+ string legalEntityId;
+ string creditRatingAgencyName;
+ uint creditRatingAmount;
+ uint currentOutstandingCreditBorrowing;
+ string currency;
+ string creditRating;
+ string docHash;
+
+ uint creditRatingIssuanceDate;
+ uint creditRatingEffectiveDate;
+ uint creditRatingExpiryDate;
+
+ uint version;
+ string modifiedBy;
+ uint lastModifiedDate;
+ string status;
+
+ function LegalEntityCreditRatingDocument(string _legalEntityId, string _creditRatingAgencyName, uint _creditRatingAmount,
+    string _creditRating, uint _creditRatingIssuanceDate, uint _creditRatingEffectiveDate, uint _creditRatingExpiryDate,
+    string _docHash){
+
+    legalEntityId=_legalEntityId;
+    creditRatingAgencyName=_creditRatingAgencyName;
+    creditRatingAmount=_creditRatingAmount;
+    creditRating=_creditRating;
+    creditRatingIssuanceDate=_creditRatingIssuanceDate;
+    creditRatingEffectiveDate=_creditRatingEffectiveDate;
+    creditRatingExpiryDate=_creditRatingExpiryDate;
+    docHash=_docHash;
+
+    //default values
+
+    currentOutstandingCreditBorrowing=0;
+    version=0;
+    currency="INR";
+    lastModifiedDate=now;
+    status="ACTIVE";
+
+ }
+
+ function fetchCRDetails() constant returns(string _legalEntityId, string _creditRatingAgencyName, uint _creditRatingAmount, uint _currentOutstandingCreditBorrowing, string _currency, string _creditRating, string _docHash){
+
+    return (legalEntityId, creditRatingAgencyName, creditRatingAmount, currentOutstandingCreditBorrowing, currency, creditRating, docHash);
+
+ }
+
+ function fetchCRDates() constant returns(uint _creditRatingIssuanceDate, uint _creditRatingEffectiveDate, uint _creditRatingExpiryDate){
+
+    return (creditRatingIssuanceDate, creditRatingEffectiveDate, creditRatingExpiryDate);
+ }
+
+ function fetchCRStatus() constant returns(uint _version, string _modifiedBy, uint _lastModifiedDate, string _status){
+
+    return (version, modifiedBy, lastModifiedDate, status);
+ }
+
+}
+
